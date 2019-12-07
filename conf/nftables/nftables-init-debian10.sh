d3848c011964b4a350d84f5db49aac652b1dd3a9
SUGIMOTO Norimitsu <dictoss@live.jp>
2019-12-08 01:07:01 +0900

    add allow ntp.

--- conf/nftables/nftables-init-debian10.sh
+++ conf/nftables/nftables-init-debian10.sh
@@ -6,6 +6,7 @@ nft add rule ip filter INPUT ct state invalid drop
 nft add rule ip filter INPUT iif lo accept
 nft add rule ip filter INPUT ip protocol icmp icmp type { echo-request, echo-reply, destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
 nft add rule ip filter INPUT tcp dport ssh ip saddr { 192.168.0.0-192.168.255.255, 172.16.0.0-172.16.255.255 , 10.0.0.0-10.255.255.255 } accept
+nft add rule ip filter INPUT udp dport ntp ip saddr { 10.0.0.0/8, 172.16.0.0/16, 192.168.0.0/16 } accept
 nft add rule ip filter INPUT tcp dport { http, https } ip saddr { 0.0.0.0/0 } accept
 nft chain ip filter INPUT { policy drop \; }
 
