d57c83740505c9419db2f77c598e55d5fb8ca904
SUGIMOTO Norimitsu <dictoss@live.jp>
2019-12-15 17:08:57 +0900

    change chain name to default.

--- conf/nftables/nftables-init-debian10.sh
+++ conf/nftables/nftables-init-debian10.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # IPv4
-nft add rule ip filter INPUT ct state established,related accept
-nft add rule ip filter INPUT ct state invalid drop
-nft add rule ip filter INPUT iif lo accept
-nft add rule ip filter INPUT ip protocol icmp icmp type { echo-request, echo-reply, destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
-nft add rule ip filter INPUT tcp dport ssh ip saddr { 192.168.0.0-192.168.255.255, 172.16.0.0-172.16.255.255 , 10.0.0.0-10.255.255.255 } accept
-nft add rule ip filter INPUT udp dport ntp ip saddr { 10.0.0.0/8, 172.16.0.0/16, 192.168.0.0/16 } accept
-nft add rule ip filter INPUT tcp dport { http, https } ip saddr { 0.0.0.0/0 } accept
-nft chain ip filter INPUT { policy drop \; }
+nft add rule ip filter input ct state established,related accept
+nft add rule ip filter input ct state invalid drop
+nft add rule ip filter input iif lo accept
+nft add rule ip filter input ip protocol icmp icmp type { echo-request, echo-reply, destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
+nft add rule ip filter input tcp dport ssh ip saddr { 192.168.0.0-192.168.255.255, 172.16.0.0-172.16.255.255 , 10.0.0.0-10.255.255.255 } accept
+nft add rule ip filter input udp dport ntp ip saddr { 10.0.0.0/8, 172.16.0.0/16, 192.168.0.0/16 } accept
+nft add rule ip filter input tcp dport { http, https } ip saddr { 0.0.0.0/0 } accept
+nft chain ip filter input { policy drop \; }
 
 # IPv6
-nft add rule ip6 filter INPUT ct state established,related accept
-nft add rule ip6 filter INPUT ct state invalid drop
-nft add rule ip6 filter INPUT iif lo accept
-nft chain ip6 filter INPUT { policy drop \; }
+nft add rule ip6 filter input ct state established,related accept
+nft add rule ip6 filter input ct state invalid drop
+nft add rule ip6 filter input iif lo accept
+nft chain ip6 filter input { policy drop \; }
