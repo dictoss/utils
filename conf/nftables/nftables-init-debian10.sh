4180d489f0d7847d50a4a5af941dfedd98aa8f0c
Norimitsu Sugimoto <dictoss@live.jp>
2019-12-31 17:49:06 +0900

    fix class b network address range.

--- conf/nftables/nftables-init-debian10.sh
+++ conf/nftables/nftables-init-debian10.sh
@@ -11,8 +11,8 @@ nft add rule ip filter input ct state established,related accept
 nft add rule ip filter input ct state invalid drop
 nft add rule ip filter input iif lo accept
 nft add rule ip filter input ip protocol icmp icmp type { echo-request, echo-reply, destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
-nft add rule ip filter input tcp dport ssh ip saddr { 192.168.0.0-192.168.255.255, 172.16.0.0-172.16.255.255 , 10.0.0.0-10.255.255.255 } accept
-nft add rule ip filter input udp dport ntp ip saddr { 10.0.0.0/8, 172.16.0.0/16, 192.168.0.0/16 } accept
+nft add rule ip filter input tcp dport ssh ip saddr { 192.168.0.0-192.168.255.255, 172.16.0.0-172.31.255.255 , 10.0.0.0-10.255.255.255 } accept
+nft add rule ip filter input udp dport ntp ip saddr { 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16 } accept
 nft add rule ip filter input tcp dport { http, https } ip saddr { 0.0.0.0/0 } accept
 nft chain ip filter input { policy drop \; }
 
